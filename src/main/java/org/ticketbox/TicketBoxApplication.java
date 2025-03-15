740c94b3569c691694cb7f51a42bf854aa61a6cc
PhamCongMinh <minhctthvn@gmail.com>
2025-03-15 12:24:17 +0700

    feat: register event service with eureka server

--- src/main/java/org/ticketbox/TicketBoxApplication.java
+++ src/main/java/org/ticketbox/TicketBoxApplication.java
@@ -3,11 +3,13 @@ package org.ticketbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
 public class TicketBoxApplication {
 
